795ba61fbf960e2e0ef429b8a42a5b4350a40449
Clark-Griswold <dustindavis1@gmail.com>
2019-11-17 17:51:40 +0000

    finished quiz

--- app/controllers/puns_controller.rb
+++ app/controllers/puns_controller.rb
@@ -6,4 +6,15 @@ class PunsController < ApplicationController
   def new
     @pun = Pun.new
   end
+
+  def create
+    Pun.create(pun_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def pun_params
+    params.require(:pun).permit(:joke)
+  end
 end
